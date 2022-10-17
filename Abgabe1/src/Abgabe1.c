81ec545307d50aafd4e5de0426369bc191730dea
student <student@ubuntu>
2022-10-17 19:37:37 +0200

    Zweiter Versuch mit C Projekt

---
+++ Abgabe1/src/Abgabe1.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Abgabe1.c
+ Author      : Mario Wegmann
+ Version     :
+ Copyright   : Hochschule Augsburg
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World from Mario!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
