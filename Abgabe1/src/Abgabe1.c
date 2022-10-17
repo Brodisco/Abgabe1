90e8fcf8e0f8e991a2e61dbd10b1a75938f057d3
student <student@ubuntu>
2022-10-17 19:41:51 +0200

    Neuer Text

--- Abgabe1/src/Abgabe1.c
+++ Abgabe1/src/Abgabe1.c
@@ -12,6 +12,6 @@
 #include <stdlib.h>
 
 int main(void) {
-	puts("!!!Hello World from Mario!!!"); /* prints !!!Hello World!!! */
+	puts("Hello World from Mario"); /* prints !!!Hello World!!! */
 	return EXIT_SUCCESS;
 }
