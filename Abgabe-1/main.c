00b9202f9115c3657f24ccf7015f84150cf73494
bklaric01 <bklaric01@bklaric>
2022-10-18 21:38:48 +0200

    Probe 2

--- Abgabe-1/main.c
+++ Abgabe-1/main.c
@@ -5,5 +5,59 @@
  *      Author: student
  */
 
-include <stdio.h>
+/*
+ * main.c
+ *
+ *  Created on: 17.10.2022
+ *      Author: student
+ */
 
+//Aufgabe 1
+
+#include <stdio.h>
+#include <stdlib.h>
+/*
+int main(void)
+{
+	printf("   (\"`-''-/\").___..--''\"`-._\n");
+	printf("    `6_ 6 )      `-. (    ). `-.__.`)\n");
+	printf("    (_Y_.)'   ._   ) `._ `. ``-..-'\n");
+	printf("   _..`--'_..-_/  /--'_.' ,'\n");
+	printf("  (il),-''  (li),'  ((!.-'");
+	return EXIT_SUCCESS;
+}
+*/
+//Aufgabe 2
+
+/*
+int main(void)
+{
+	int Nummer;
+	printf("Geben Sie ein vierstelligen Nummer ein: ");
+	scanf("%d", &Nummer);
+
+	if (Nummer<10000 && Nummer>=1000)
+	{
+	int ErsteStelle = Nummer / 1000;
+	int ZweiteStelle = Nummer / 100 - ErsteStelle * 10;
+	int DritteStelle = Nummer / 10 - ErsteStelle * 100 - ZweiteStelle * 10;
+	int VierteStelle = Nummer - ErsteStelle * 1000 - ZweiteStelle * 100 - DritteStelle * 10;
+
+	int Quersumme = ErsteStelle + ZweiteStelle + DritteStelle + VierteStelle;
+	printf("\n");
+	printf("Die Quersumme ist: %d", Quersumme);
+	}
+
+	else
+	printf("Das ist nicht ein vierstelligen Nummer...");
+
+	return EXIT_SUCCESS;
+}
+*/
+
+//Aufgabe 3
+
+int main(void)
+{
+
+}
