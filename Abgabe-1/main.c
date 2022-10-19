7376a6443e2d53d24b264bab9844f4ceea1612b2
benjaminklaric <benjaminklaric@pool8021x172-174.rz.hs-augsburg.de>
2022-10-19 16:45:04 +0200

    Aufgabe 3 + 4 (Benjamin)

--- Abgabe-1/main.c
+++ Abgabe-1/main.c
@@ -1,16 +1,3 @@
-/*
- * main.c
- *
- *  Created on: 17.10.2022
- *      Author: student
- */
-
-/*
- * main.c
- *
- *  Created on: 17.10.2022
- *      Author: student
- */
 
 //Aufgabe 1
 
@@ -58,8 +45,61 @@ int main(void)
 */
 
 //Aufgabe 3
-
+//a)
+/*
 int main(void)
+{
+	int stunden = 12;
+	int minuten = 20;
+	int sekunden = 43;
+
+	printf("Aktuelle Uhrzeit:    %d:%d:%d Uhr", stunden, minuten, sekunden);
+	return EXIT_SUCCESS;
+}
+*/
+//b)
+/*
+int main(void)
+{
+	int stunden = 12;
+	int minuten = 20;
+	int sekunden = 34;
+
+	printf("Aktuelle Uhrzeit:    %02d:%02d:%02d Uhr", stunden, minuten, sekunden);
+	return EXIT_SUCCESS;
+}
+*/
+
+//Aufgabe 4
+//a)
+/*
+int main (void)
+{
+	int x  = 24060;
+	int stunden = x / 3600;
+	int minuten = x / 60 - stunden * 60;
+	int sekunden = x - minuten * 60 - stunden * 3600;
+
+	printf("Prof. von Bodisco steht um  %2d:%02d:%02d Uhr auf.", stunden, minuten, sekunden);
+	return EXIT_SUCCESS;
+}
+*/
+
+//b)
+/*
+int main (void)
+{
+	int x  = 24060;
+
+	printf("Prof. von Bodisco steht um  %2d:%02d:%02d Uhr auf.", x / 3600, x / 60 - x / 3600 * 60, x - (x / 60 - x / 3600 * 60) * 60 - x / 3600 * 3600);
+	return EXIT_SUCCESS;
+}
+*/
+
+//Aufgabe 5
+
+int main (void)
 {
 
 }
+
