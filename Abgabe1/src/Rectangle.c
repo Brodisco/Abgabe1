2ff5b0db2553ca058c24ed5a29d7c03fa4670bf5
Brodisco <115808535+Brodisco@users.noreply.github.com>
2022-10-19 16:43:34 +0200

    Create Rectangle.c

---
+++ Abgabe1/src/Rectangle.c
@@ -0,0 +1,26 @@
+/*
+ * Rectangle.c
+ *
+ *  Created on: 19.10.2022
+ *      Author: student
+ */
+
+void rechteck(unsigned int breite, unsigned int hoehe, char c) {
+   // Obere Seite
+   for (int i = 0; i < breite; i++) {
+       printf("%c", c);
+   }
+   printf("\n");
+   
+   // Koerper 
+   
+   for (int k = 0; k < hoehe - 2; k++) { 
+       printf("%*c%c\n", -breite+1, c, c);
+   }
+   
+   // Untere Seite
+   for (int i = 0; i < breite; i++) {
+       printf("%c", c);
+   }
+   printf("\n");
+}
