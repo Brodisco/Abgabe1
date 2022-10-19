be0cd94b653d1f1409c54c49b38852c96d4013b4
benjaminklaric <benjaminklaric@pool8021x172-174.rz.hs-augsburg.de>
2022-10-19 22:30:22 +0200

    Aufgaben 1-6 (Benjamin)

--- Abgabe-1/main.c
+++ Abgabe-1/main.c
@@ -2,24 +2,22 @@
 //Aufgabe 1
 
 #include <stdio.h>
-
-
 #include <stdlib.h>
-/*
-int main(void)
+
+
+void PrintTiger(void)
 {
 	printf("   (\"`-''-/\").___..--''\"`-._\n");
 	printf("    `6_ 6 )      `-. (    ). `-.__.`)\n");
 	printf("    (_Y_.)'   ._   ) `._ `. ``-..-'\n");
 	printf("   _..`--'_..-_/  /--'_.' ,'\n");
 	printf("  (il),-''  (li),'  ((!.-'");
-	return EXIT_SUCCESS;
 }
-*/
+
 //Aufgabe 2
 
-/*
-int main(void)
+
+void QuersummmeBerechnen(void)
 {
 	int Nummer;
 	printf("Geben Sie ein vierstelligen Nummer ein: ");
@@ -27,53 +25,47 @@ int main(void)
 
 	if (Nummer<10000 && Nummer>=1000)
 	{
-	int ErsteStelle = Nummer / 1000;
-	int ZweiteStelle = Nummer / 100 - ErsteStelle * 10;
-	int DritteStelle = Nummer / 10 - ErsteStelle * 100 - ZweiteStelle * 10;
-	int VierteStelle = Nummer - ErsteStelle * 1000 - ZweiteStelle * 100 - DritteStelle * 10;
+		int ErsteStelle = Nummer / 1000;
+		int ZweiteStelle = Nummer / 100 - ErsteStelle * 10;
+		int DritteStelle = Nummer / 10 - ErsteStelle * 100 - ZweiteStelle * 10;
+		int VierteStelle = Nummer - ErsteStelle * 1000 - ZweiteStelle * 100 - DritteStelle * 10;
 
-	int Quersumme = ErsteStelle + ZweiteStelle + DritteStelle + VierteStelle;
-	printf("\n");
-	printf("Die Quersumme ist: %d", Quersumme);
-	}
+		int Quersumme = ErsteStelle + ZweiteStelle + DritteStelle + VierteStelle;
+		printf("\n");
+		printf("Die Quersumme ist: %d", Quersumme);
+	} else
+		printf("Das ist nicht ein vierstelligen Nummer...");
 
-	else
-	printf("Das ist nicht ein vierstelligen Nummer...");
-
-	return EXIT_SUCCESS;
 }
-*/
+
 
 //Aufgabe 3
 //a)
-/*
-int main(void)
+
+void Uhrzeit1(void)
 {
 	int stunden = 12;
 	int minuten = 20;
 	int sekunden = 43;
 
 	printf("Aktuelle Uhrzeit:    %d:%d:%d Uhr", stunden, minuten, sekunden);
-	return EXIT_SUCCESS;
 }
-*/
 //b)
-/*
-int main(void)
+
+void Uhrzeit2(void)
 {
 	int stunden = 12;
 	int minuten = 20;
 	int sekunden = 34;
 
 	printf("Aktuelle Uhrzeit:    %02d:%02d:%02d Uhr", stunden, minuten, sekunden);
-	return EXIT_SUCCESS;
 }
-*/
+
 
 //Aufgabe 4
 //a)
-/*
-int main (void)
+
+void Uhrzeit3 (void)
 {
 	int x  = 24060;
 	int stunden = x / 3600;
@@ -81,25 +73,81 @@ int main (void)
 	int sekunden = x - minuten * 60 - stunden * 3600;
 
 	printf("Prof. von Bodisco steht um  %2d:%02d:%02d Uhr auf.", stunden, minuten, sekunden);
-	return EXIT_SUCCESS;
 }
-*/
+
 
 //b)
-/*
-int main (void)
+
+void Uhrzeit4 (void)
 {
 	int x  = 24060;
 
 	printf("Prof. von Bodisco steht um  %2d:%02d:%02d Uhr auf.", x / 3600, x / 60 - x / 3600 * 60, x - (x / 60 - x / 3600 * 60) * 60 - x / 3600 * 3600);
-	return EXIT_SUCCESS;
+
 }
-*/
+
 
 //Aufgabe 5
 
-int main (void)
+void Pinnummer (void)
+{
+	int Nummer = 0;
+	while (Nummer < 9999)
+	{
+		Nummer++;
+		printf("%0004d\n", Nummer);
+	}
+}
+
+//Aufgabe 6
+
+void Palindrom (void)
+{
+	int Nummer;
+		printf("Geben Sie ein funfstelligen Nummer ein: ");
+		scanf("%d", &Nummer);
+
+		if (Nummer>9999 && Nummer<100000)
+		{
+			int ErsteStelle = Nummer / 10000;
+			int ZweiteStelle = Nummer / 1000 - ErsteStelle * 10;
+			int DritteStelle = Nummer / 100 - ErsteStelle * 100 - ZweiteStelle * 10;
+			int VierteStelle = Nummer / 10 - ErsteStelle * 1000 - ZweiteStelle * 100 - DritteStelle * 10;
+			int FunfteStelle = Nummer - ErsteStelle * 10000 - ZweiteStelle * 1000 - DritteStelle * 100 - VierteStelle *10;
+
+			if (ErsteStelle == FunfteStelle && ZweiteStelle == VierteStelle)
+			{
+			printf("\n");
+			printf("Nummer %d ist ein Palindrom", Nummer);
+			}
+			else
+			printf("Nummer %d ist kein Palindrom", Nummer);
+
+		}
+		else
+		printf("Das ist nicht ein funfstelligen Nummer...");
+}
+
+//Aufgabe 7
+
+void Rechteck (void)
 {
 
 }
+
+int main (void)
+{
+//	PrintTiger();
+//	QuersummmeBerechnen();
+//	Uhrzeit1();
+//	Uhrzeit2();
+//	Uhrzeit3();
+//	Uhrzeit4();
+//	Pinnummer();
+//	Palindrom();
+	Rechteck();
+
+	return EXIT_SUCCESS;
+}
+
 
